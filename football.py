0b1e98aeb9b347f71af9f0d5ee083ecfe17bf800
Jason Williamson <42687824+jasonwnc@users.noreply.github.com>
2021-09-13 15:53:34 -0400

    Update football.py

--- football.py
+++ football.py
@@ -26,7 +26,8 @@ with open('College Football Seasons 2014-2020.csv','r') as read_obj:
     		fieldnames = (fieldnames + ' ' + str(column_names[i]) + ' varchar(255) ')
     	i=i+1
         #end the loop
-sql= (sqlval + fieldnames + ');')
+        new_string = fieldnames.replace(".", "_")
+sql= (sqlval + new_string + ');')
 cursor.execute(sql)
 cnx.commit()
 cursor.close()
